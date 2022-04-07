9938818d109a6ffa78eca90a733283f17a537402
Tony Krikorian <tony.krikorian@anschile.cl>
2022-04-07 18:35:33 -0400

    Add routing dynamic

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '../services/EventService'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null
+    }
+  },
+  async created() {
+    const response = await EventService.getEvent(this.id)
+    this.event = response.data
+  }
+}
+</script>
